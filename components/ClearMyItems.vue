5103532eccf811ce05f2ce2af14256493facccb0
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2021-07-29 18:11:34 -0700

    updated

---
+++ components/ClearMyItems.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <b-button
+      class="mt-3"
+      variant="outline-danger"
+      @click="
+        () => {
+          clearMyItems();
+        }
+      "
+      >Clear All</b-button
+    >
+  </div>
+</template>
+
+<script>
+import { mapMutations } from "vuex";
+export default {
+  methods: {
+    ...mapMutations(["clearMyItems"])
+  }
+};
+</script>
+
+<style scoped></style>
