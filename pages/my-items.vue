67112ac7a6b65e8f1032e1a1f2828b15d8c6c961
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2021-07-29 18:56:53 -0700

    my items updated

--- pages/my-items.vue
+++ pages/my-items.vue
@@ -1,7 +1,12 @@
 <template>
-  <div v-if="myRentals.length" class="container">
-    <ClearMyItems />
-    <MyItem v-for="item in myRentals" :key="item.id" :item="item" />
+  <div class="container">
+    <div v-if="myRentals.length">
+      <ClearMyItems />
+      <MyItem v-for="item in myRentals" :key="item.id" :item="item" />
+    </div>
+    <div class="no-items container" v-else>
+      <h2>No items here yet.</h2>
+    </div>
   </div>
 </template>
 
@@ -14,3 +19,12 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.no-items {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50vh;
+}
+</style>
