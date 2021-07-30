d16766d04058f203d526f07e9645612d2e3d686c
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2021-07-29 17:56:42 -0700

    project finished

---
+++ layouts/no-nav.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <nuxt-link to="/">Go Back</nuxt-link>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.container {
+  padding: 5rem 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="container">
+      <Hero />
+      <LargeCardDisplay
+        v-for="cardInfo in largeCardInfo"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+      />
+      <SmallCardDisplay
+        v-for="cardInfo in smallCardSections"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+// import Hero from "@/components/Hero";
+// import LargeCardDisplay from "@/components/LargeCardDisplay";
+
+import { largeCardSections, smallCardSections } from "@/assets/data.js";
+
+export default {
+  data() {
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardSections
+    };
+  }
+  // components: {
+  //   Hero,
+  //   LargeCardDisplay
+  // }
+};
+</script>
